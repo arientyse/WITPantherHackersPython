3a45fb47b9eac8deaecd9033e2aedae87524fc1a
Ariana Tyson <arianatyson@live.com>
2019-02-07 15:25:57 -0500

    Added print statement

--- module1.py
+++ module1.py
@@ -1,6 +1,12 @@
 import random
 
+#import is a variable
+#random is a library
+
 number_of_guesses = 0
 number_to_guess = random.randint(1,15)
 
+#Use the pound sign to make comments
 
+guess = input()
+print("Please enter a number for me to guess")
