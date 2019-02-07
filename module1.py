634c42a7d6004fa98c19670dee93687b59dce2aa
Ariana Tyson <arianatyson@live.com>
2019-02-07 15:35:13 -0500

    Added if statements

--- module1.py
+++ module1.py
@@ -8,5 +8,15 @@ number_to_guess = random.randint(1,15)
 
 #Use the pound sign to make comments
 
-guess = input()
-print("Please enter a number for me to guess")
+print("Please enter a number for me to guess!") #Print line
+guess = input() #Computer is going to do logic to guess the user's input
+
+#Control Flow: If, If...else, If...elif
+
+if guess < number_to_guess:
+    print("That's too low!")
+if guess > number_to_guess:
+    print("That's way too high!")
+if guess == number_to_guess:
+    print("YAY! You guessed the right answer!")
+
